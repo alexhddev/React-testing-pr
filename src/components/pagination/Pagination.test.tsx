56b3f3f4a46fa94520946d3a3a19662dbaab157c
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-20 14:40:28 +0200

    pagination tests

---
+++ src/components/pagination/Pagination.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import Pagination from "./Pagination";
+
+describe("Pagination", () => {
+  it("renders correct pagination", () => {
+    const handleClick = vi.fn();
+    render(<Pagination total={50} limit={10} currentPage={1} selectPage={handleClick}/>);
+    const pageContainers = screen.getAllByTestId("page-container");
+    expect(pageContainers).toHaveLength(5);
+    expect(pageContainers[0]).toHaveTextContent("1");
+  });
+
+  it("should emit clicked page", async () => {
+    const user = userEvent.setup();
+    const handleClick = vi.fn();
+    render(
+      <Pagination
+        total={50}
+        limit={10}
+        currentPage={1}
+        selectPage={handleClick}
+      />
+    );
+    const pageContainers = screen.getAllByTestId("page-container");
+    await user.click(pageContainers[0]);
+    expect(handleClick).toHaveBeenCalledWith(1);
+  });
+});
