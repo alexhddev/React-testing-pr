7f7567c7b6fd01b911bdc0a2bcdd93f7e06321e4
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-20 10:52:04 +0200

    test setup

---
+++ src/Utils.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+describe('utils test suite', ()=>{
+    it('check if 1 = 1', ()=>{
+        expect(1).toEqual(1)
+    })
+
+    it('check if 2 = 2', ()=>{
+        expect(2).toEqual(2)
+    })
+})
