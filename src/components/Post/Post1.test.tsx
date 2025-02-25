7611805da2c8ccd5957dee32364535d4578cdb56
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-25 08:45:29 +0200

    basic test

--- src/components/Post/Post1.test.tsx
+++ src/components/Post/Post1.test.tsx
@@ -1,15 +1,25 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen,  } from "@testing-library/react"
 import { Post1 } from "./Post1"
 
 
 describe('Post 1 test suite', ()=>{
     it('should be rendered in the document', ()=>{
+        const someUserName = 'Alex';
+        const someContent = 'Some content'
         render(<Post1
-            user="Alex"
-            content="Some content"
+            user={someUserName}
+            content={someContent}
         ></Post1>)
 
         const postContainer = screen.getByTestId('post-container')
         expect(postContainer).toBeInTheDocument();
+
+        const user = screen.getByRole('heading')
+        expect(user).toBeInTheDocument();
+        expect(user).toHaveTextContent(someUserName)
+
+        const postContent = screen.getByRole('paragraph')
+        expect(postContent).toBeInTheDocument();
+        expect(postContent).toHaveTextContent(someContent)
     })
 })
