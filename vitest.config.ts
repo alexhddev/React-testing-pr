ee022d243e2438e4601e1364d13b2bb5b0cc443e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-26 15:54:25 +0200

    add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,14 @@ export default defineConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './test/setup'
-    },
+      setupFiles: './test/setup',
+      coverage: {
+        include:[
+          'src/**/*.{js,jsx,ts,tsx}'
+        ],
+        exclude: [
+          'src/**/*.test.{ts,tsx}'
+        ]
+      }
+    }
   })
