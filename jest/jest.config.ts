ed2361da9a5169d882f245674553f550baeac1a4
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-03-03 14:33:06 +0200

    extending jest setup - jsx is not set error

---
+++ jest/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    rootDir: './',
+    // preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+        // process `*.tsx` files with `ts-jest`
+    },
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/mocks/fileMock.js',
+    },
+}
+
+export default config
