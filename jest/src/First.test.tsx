0406dd0479e9266eb10b2960ed733347dc7da02a
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-03-03 11:58:02 +0200

    jest initial config - unexpected token error

---
+++ jest/src/First.test.tsx
@@ -0,0 +1,10 @@
+import { First } from './First'
+import { render } from "@testing-library/react"
+
+describe('First tests', ()=>{
+  it('Should render component', ()=>{
+    render(<First />)
+    expect(true).toBeTruthy()
+
+  })
+})
