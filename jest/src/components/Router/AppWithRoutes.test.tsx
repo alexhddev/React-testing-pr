8ff33dfdb9b27d25d508ac3540ae7bf03febf506
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-03-12 11:40:03 +0200

    Working click tests

--- jest/src/components/Router/AppWithRoutes.test.tsx
+++ jest/src/components/Router/AppWithRoutes.test.tsx
@@ -2,10 +2,21 @@ import { TextEncoder } from 'util';
 global.TextEncoder = TextEncoder;
 import { render, screen } from '@testing-library/react'
 import { AppWithRoutes } from './AppWithRoutes'
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event'
+
 
 
 jest.mock('./Home', () => ({
     Home: () => <div data-testid='HomeMock' />
+}))
+
+jest.mock('./About', () => ({
+    About: () => <div data-testid='AboutMock' />
+}))
+
+jest.mock('./Posts', () => ({
+    Posts: () => <div data-testid='PostsMock' />
 }))
 
 describe('App with routes test suite', () => {
@@ -19,5 +30,46 @@ describe('App with routes test suite', () => {
         render(<AppWithRoutes />)
         const home = screen.getByTestId('HomeMock')
         expect(home).toBeInTheDocument();
+    })
+
+    it.skip('Should load routes - about route', () => {
+
+        render(
+            // won't work, Router inside a router
+            <MemoryRouter initialEntries={['/about']}>
+                <AppWithRoutes />
+            </MemoryRouter>)
+    })
+
+    describe('Navbar navigation tests', () => {
+        // mock request in order to test
+        global["Request"] = jest.fn().mockImplementation(() => ({
+            signal: {
+                removeEventListener: () => { },
+                addEventListener: () => { },
+            },
+        }));
+        
+        it('show home component on home click', async () => {
+            render(<AppWithRoutes />)
+            const user = userEvent.setup()
+            const homeButton = screen.getByText('Home');
+
+            await user.click(homeButton);
+
+            const home = screen.getByTestId('HomeMock')
+            expect(home).toBeInTheDocument();
+        })
+
+        it('show about component on about click', async () => {
+            render(<AppWithRoutes />)
+            const user = userEvent.setup()
+            const aboutButton = screen.getByText('About');
+
+            await user.click(aboutButton);
+
+            const home = screen.getByTestId('AboutMock')
+            expect(home).toBeInTheDocument();
+        })
     })
 })
