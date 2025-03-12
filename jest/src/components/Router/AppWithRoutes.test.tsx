a4a477084a52206484c5281f1cdeeab49874ca4e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-03-12 09:44:15 +0200

    routing testing setup

---
+++ jest/src/components/Router/AppWithRoutes.test.tsx
@@ -0,0 +1,27 @@
+
+import { TextEncoder } from 'util';
+global.TextEncoder = TextEncoder;
+
+import { BrowserRouter, MemoryRouter } from 'react-router'
+import { render, screen } from '@testing-library/react'
+import { AppWithRoutes } from './AppWithRoutes'
+
+jest.mock('./Home', () => ({
+    Home: () => <div data-testid='HomeMock' />
+}))
+
+
+describe('App with routes test suite', () => {
+    it('Should always load the navbar', () => {
+        render(<AppWithRoutes />)
+        const navbar = screen.getByTestId('NavBar')
+        expect(navbar).toBeInTheDocument();
+    })
+
+    it('Should initially load the home component', () => {
+        render(<AppWithRoutes />)
+        const home = screen.getByTestId('HomeMock')
+        expect(home).toBeInTheDocument();
+    })
+
+})
--- jest/src/components/Router/Navbar.tsx
+++ jest/src/components/Router/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router'
 
 export default function NavBar() {
 
-    return <div className="navbar">
+    return <div className="navbar" data-testid='NavBar'>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/about"}>About</NavLink>
         <NavLink to={"/posts"}>Posts</NavLink>
